ddbc42ee51d2996e9bfbe8261fe9ad2fbc165f90
malikid <malikid.chang@gmail.com>
2018-04-13 02:05:00 +0800

    Updated build config.

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,6 @@
 module.exports = {
   configureWebpack: {
     output: {
-      // path: '/Volumes/HD/works/contributes/mk-miho/dist',
-      // filename: '[name].js',
       publicPath: '/mk-miho/',
     },
   },
